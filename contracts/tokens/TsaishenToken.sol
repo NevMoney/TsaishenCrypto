ec052bcf13bc3cde196f8147ca22967c9e640fff
Nev <67838965+NevMoney@users.noreply.github.com>
2020-11-23 18:54:58 -0500

    team token
    started code for team tokens locked

--- contracts/tokens/TsaishenToken.sol
+++ contracts/tokens/TsaishenToken.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.6.10;
 
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
+import "../Storage.sol";
 
-contract TsaishenToken is ERC20PresetMinterPauser {
+contract TsaishenToken is ERC20PresetMinterPauser, Storage {
     
     constructor() public ERC20PresetMinterPauser("Tsaishen Coin", "CASH") {
         _mint(msg.sender, 100e6 * 1 ether); //1 ether - same as saying 18 decimals
