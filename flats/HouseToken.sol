1b86e7e7563e3bbe429a6440dbf211c94e72db2b
Nev <67838965+NevMoney@users.noreply.github.com>
2020-12-03 12:51:22 -0500

    houseToken flat

--- flats/HouseToken.sol
+++ flats/HouseToken.sol
@@ -246,7 +246,7 @@ library EnumerableSet {
 
 // File: node_modules\@openzeppelin\contracts\utils\Address.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.2;
 
@@ -390,7 +390,7 @@ library Address {
 
 // File: node_modules\@openzeppelin\contracts\GSN\Context.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -417,7 +417,7 @@ abstract contract Context {
 
 // File: node_modules\@openzeppelin\contracts\access\AccessControl.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -636,7 +636,7 @@ abstract contract AccessControl is Context {
 
 // File: node_modules\@openzeppelin\contracts\math\SafeMath.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -798,7 +798,7 @@ library SafeMath {
 
 // File: node_modules\@openzeppelin\contracts\utils\Counters.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -840,7 +840,7 @@ library Counters {
 
 // File: node_modules\@openzeppelin\contracts\introspection\IERC165.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -867,7 +867,7 @@ interface IERC165 {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\IERC721.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.2;
 
@@ -998,7 +998,7 @@ interface IERC721 is IERC165 {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\IERC721Metadata.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.2;
 
@@ -1027,7 +1027,7 @@ interface IERC721Metadata is IERC721 {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\IERC721Enumerable.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.2;
 
@@ -1058,7 +1058,7 @@ interface IERC721Enumerable is IERC721 {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\IERC721Receiver.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1083,7 +1083,7 @@ interface IERC721Receiver {
 
 // File: node_modules\@openzeppelin\contracts\introspection\ERC165.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1139,7 +1139,7 @@ contract ERC165 is IERC165 {
 
 // File: node_modules\@openzeppelin\contracts\utils\EnumerableMap.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1379,7 +1379,7 @@ library EnumerableMap {
 
 // File: node_modules\@openzeppelin\contracts\utils\Strings.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1416,7 +1416,7 @@ library Strings {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\ERC721.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1891,7 +1891,7 @@ contract ERC721 is Context, ERC165, IERC721, IERC721Metadata, IERC721Enumerable 
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\ERC721Burnable.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -1918,7 +1918,7 @@ abstract contract ERC721Burnable is Context, ERC721 {
 
 // File: node_modules\@openzeppelin\contracts\utils\Pausable.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -2010,7 +2010,7 @@ contract Pausable is Context {
 
 // File: node_modules\@openzeppelin\contracts\token\ERC721\ERC721Pausable.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -2040,7 +2040,7 @@ abstract contract ERC721Pausable is ERC721, Pausable {
 
 // File: @openzeppelin\contracts\presets\ERC721PresetMinterPauserAutoId.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -2144,7 +2144,7 @@ contract ERC721PresetMinterPauserAutoId is Context, AccessControl, ERC721Burnabl
 
 // File: @openzeppelin\contracts\access\Ownable.sol
 
-
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
@@ -2212,12 +2212,258 @@ contract Ownable is Context {
     }
 }
 
-// File: contracts\Storage.sol
+// File: @openzeppelin\contracts\utils\ReentrancyGuard.sol
 
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
 
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor () internal {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+
+// File: contracts\CRUD.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+/*
+Hitchens UnorderedAddressSet v0.93
+Library for managing CRUD operations in dynamic address sets.
+https://github.com/rob-Hitchens/UnorderedKeySet
+Copyright (c), 2019, Rob Hitchens, the MIT License
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+THIS SOFTWARE IS NOT TESTED OR AUDITED. DO NOT USE FOR PRODUCTION.
+*/
+
+/**
+ * @notice Key sets with enumeration and delete. Uses mappings for random
+ * and existence checks and dynamic arrays for enumeration. Key uniqueness is enforced. 
+ * @dev Sets are unordered. Delete operations reorder keys. All operations have a 
+ * fixed gas cost at any scale, O(1). 
+ * author: Rob Hitchens
+ */
+
+library AddressSet {
+    
+    struct Set {
+        mapping(address => uint) keyPointers;
+        address[] keyList;
+    }
+
+    /**
+     * @notice insert a key. 
+     * @dev duplicate keys are not permitted.
+     * @param self storage pointer to a Set. 
+     * @param key value to insert.
+     */    
+    function insert(Set storage self, address key) internal {
+        require(!exists(self, key), "AddressSet: key already exists in the set.");
+        self.keyList.push(key);
+        self.keyPointers[key] = self.keyList.length -1;
+    }
+
+    /**
+     * @notice remove a key.
+     * @dev key to remove must exist. 
+     * @param self storage pointer to a Set.
+     * @param key value to remove.
+     */    
+    function remove(Set storage self, address key) internal {
+        require(exists(self, key), "AddressSet: key does not exist in the set.");
+        uint last = count(self) - 1;
+        uint rowToReplace = self.keyPointers[key];
+        if(rowToReplace != last) {
+            address keyToMove = self.keyList[last];
+            self.keyPointers[keyToMove] = rowToReplace;
+            self.keyList[rowToReplace] = keyToMove;
+        }
+        delete self.keyPointers[key];
+        self.keyList.pop();
+    }
+
+    /**
+     * @notice count the keys.
+     * @param self storage pointer to a Set. 
+     */       
+    function count(Set storage self) internal view returns(uint) {
+        return(self.keyList.length);
+    }
+
+    /**
+     * @notice check if a key is in the Set.
+     * @param self storage pointer to a Set.
+     * @param key value to check. 
+     * @return bool true: Set member, false: not a Set member.
+     */  
+    function exists(Set storage self, address key) internal view returns(bool) {
+        if(self.keyList.length == 0) return false;
+        return self.keyList[self.keyPointers[key]] == key;
+    }
+
+    /**
+     * @notice fetch a key by row (enumerate).
+     * @param self storage pointer to a Set.
+     * @param index row to enumerate. Must be < count() - 1.
+     */      
+    function keyAtIndex(Set storage self, uint index) internal view returns(address) {
+        return self.keyList[index];
+    }
+}
+
+/**
+ * @notice Key sets with enumeration and delete. Uses mappings for random
+ * and existence checks and dynamic arrays for enumeration. Key uniqueness is enforced. 
+ * @dev Sets are unordered. Delete operations reorder keys. All operations have a 
+ * fixed gas cost at any scale, O(1). 
+ * author: Rob Hitchens
+ */
+
+library UintSet {
+    
+    struct Set {
+        mapping(uint => uint) keyPointers;
+        uint[] keyList;
+    }
+
+    /**
+     * @notice insert a key. 
+     * @dev duplicate keys are not permitted.
+     * @param self storage pointer to a Set. 
+     * @param key value to insert.
+     */       
+    function insert(Set storage self, uint key) internal {
+        require(!exists(self, key), "UintSet: key already exists in the set.");
+        self.keyList.push(key);
+        self.keyPointers[key] = self.keyList.length -1;
+    }
+
+    /**
+     * @notice remove a key.
+     * @dev key to remove must exist. 
+     * @param self storage pointer to a Set.
+     * @param key value to remove.
+     */  
+    function remove(Set storage self, uint key) internal {
+        require(exists(self, key), "UintSet: key does not exist in the set.");
+        uint last = count(self) - 1;
+        uint rowToReplace = self.keyPointers[key];        
+        if(rowToReplace != last) {
+            uint keyToMove = self.keyList[last];
+            self.keyPointers[keyToMove] = rowToReplace;
+            self.keyList[rowToReplace] = keyToMove;
+        }
+        delete self.keyPointers[key];
+        self.keyList.pop();
+    }
+
+    /**
+     * @notice count the keys.
+     * @param self storage pointer to a Set. 
+     */   
+    function count(Set storage self) internal view returns(uint) {
+        return(self.keyList.length);
+    }
+
+    /**
+     * @notice check if a key is in the Set.
+     * @param self storage pointer to a Set.
+     * @param key value to check. 
+     * @return bool true: Set member, false: not a Set member.
+     */  
+    function exists(Set storage self, uint key) internal view returns(bool) {
+        if(self.keyList.length == 0) return false;
+        return self.keyList[self.keyPointers[key]] == key;
+    }
+
+    /**
+     * @notice fetch a key by row (enumerate).
+     * @param self storage pointer to a Set.
+     * @param index row to enumerate. Must be < count() - 1.
+     */      
+    function keyAtIndex(Set storage self, uint index) internal view returns(uint) {
+        return self.keyList[index];
+    }
+}
+
+// File: contracts\Storage.sol
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
 contract Storage {
+    using AddressSet for AddressSet.Set;
+    using UintSet for UintSet.Set;
 
     mapping (string => uint256) _uintStorage;
     mapping (string => address) _addressStorage;
@@ -2225,60 +2471,116 @@ contract Storage {
     mapping (string => string) _stringStorage;
     mapping (string => bytes4) _bytesStorage;
     
-    mapping (uint256 => address) public houseIndexToApproved;
-    
-
-    // this is where we'll store house information about each property for easy recall
-    mapping (uint256 => House) public houseInfo;
-
-    // store offer information
-    mapping(uint256 => Offer) public offerDetails;
-
-    // srote user information
-    mapping(address => User) public users;
-    
-
     mapping (address => uint256) private _balances;
     mapping (address => mapping (address => uint256)) private _allowances;
 
     address public _owner;
     bool public _initialized;
-    uint public balance;
-
-    Offer [] offers;
-    // User [] users;
-
-    uint256 public houseCounter;
 
     struct House {
         uint256 value;
         uint256 income;
+        // UintSet.Set offerDetails;
     }
 
+    mapping (uint256 => address) public houseIndexToApproved;
+    mapping (uint256 => House) internal houseInfo;
+
+    // marketplace & lending stuff
     struct Offer {
         address payable seller;
         uint256 price;
+        uint256 income;
+        uint256 loan;
         uint256 index;
         uint256 tokenId;
         bool active;
     }
 
+    // store offer information
+    mapping(uint256 => Offer) internal offerDetails;
+    Offer [] offers;
+
+    // user stuff
     struct User {
+        string name;
+        string emailAddress;
+        uint256 cellPhone;
         address payable user;
-        uint256 reward;
-        uint256 index;
-        bool active;
+        House house;
+        UintSet.Set homes;
+    }    
+
+    // store user information
+    mapping(address => User) internal userInfo;
+
+}
+
+// File: contracts\TsaishenUsers.sol
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.6.10;
+
+// import "./CRUD.sol";
+
+contract TsaishenUsers is Ownable, Storage {
+    using AddressSet for AddressSet.Set;
+    AddressSet.Set users;
+
+    using UintSet for UintSet.Set;
+
+    event userAdded(string, address user, bool active);
+    event userDeleted(string, address user, bool active);
+
+    function addUser (string memory _name, string memory _email, uint _phone) public onlyOwner {
+        UintSet.Set memory _homes;
+        Storage.House memory _House;
+        userInfo[msg.sender] = User(_name, _email, _phone, msg.sender, _House, _homes);
+        users.insert(msg.sender);
+
+        emit userAdded("New user added", msg.sender, true);
+    }
+
+    // this MUST be internal once testing/debuging done!
+    function addHome (address _user, uint homeId) public onlyOwner {
+        userInfo[_user].homes.insert(homeId);
+    }
+
+    function getUserHomes (address _user) public view returns (uint[] memory){
+        return userInfo[_user].homes.keyList;
+    }
+
+    function isUser(address userToSearch) public view returns(bool){
+        return users.exists(userToSearch);
+    }
+
+    function deleteUser(address userToDelete) public onlyOwner {
+        users.remove(userToDelete);
+
+        emit userDeleted("User deleted", msg.sender, false);
+    }
+
+    function userCount() public view returns(uint256){
+        return users.count();
+    }
+
+    function getAllUsers() public view returns(address[] memory) {
+        return users.keyList;
     }
 
 }
 
 // File: contracts\tokens\HouseToken.sol
 
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.6.10;
 
-pragma solidity 0.6.10;
+contract HouseToken is ERC721PresetMinterPauserAutoId, Ownable, Storage, TsaishenUsers {
+    using UintSet for UintSet.Set;
+    UintSet.Set homes;
 
-contract HouseToken is ERC721PresetMinterPauserAutoId, Ownable, Storage {
     constructor() public ERC721PresetMinterPauserAutoId("Tsaishen Real Estate", "HOUS", "https://ipfs.daonomic.com/ipfs/") {
     }
 
@@ -2286,6 +2588,10 @@ contract HouseToken is ERC721PresetMinterPauserAutoId, Ownable, Storage {
         require(msg.value >= cost);
         _;
     }
+
+    uint public balance;    
+    uint256 public houseCounter;
+
 
     event Minted(address _owner, uint256 id, string tokenURI);
 
@@ -2314,6 +2620,21 @@ contract HouseToken is ERC721PresetMinterPauserAutoId, Ownable, Storage {
         _mint(_owner, _tokenIdTracker.current());
         _tokenIdTracker.increment();
 
+        // add users and house info
+        User memory _user = User('', '', 0, msg.sender, _house, homes);
+        
+        // add user if new
+        if(isUser(msg.sender)!= true){
+            users.insert(msg.sender);
+        }
+        
+        // add house to user to get array of all homes user has THIS IS NOT WORKING!
+        userInfo[msg.sender].homes.insert(_tokenIdTracker.current());
+        addHome(_owner, _tokenIdTracker.current());
+        
+        // add user to userInfo mapping
+        userInfo[msg.sender] = _user;
+
         return _tokenIdTracker.current();
     }
 
@@ -2323,4 +2644,12 @@ contract HouseToken is ERC721PresetMinterPauserAutoId, Ownable, Storage {
         income = houseInfo[_id].income;
         uri = tokenURI(_id);
     }
+
+    function withdrawAll() public onlyOwner returns(uint){
+        uint toTransfer = balance;
+        balance = 0;
+        msg.sender.transfer(toTransfer);
+        return toTransfer;
+    }
+
 }
