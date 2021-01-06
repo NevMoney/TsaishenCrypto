bf74aa432d199a3491886e58ffb077e2415119fb
Nev <67838965+NevMoney@users.noreply.github.com>
2021-01-06 18:37:26 -0500

    adding index.js
    took the eth functionality from bundle to functionality then to index.js

--- app/assets/js/indexFunctionality.js
+++ app/assets/js/indexFunctionality.js
@@ -1,4 +1,3 @@
-//Executed when page finish loading
 $(document).ready(async () => {
     $("#market-container").hide();
     $("#deed-container").hide();
@@ -8,25 +7,8 @@ $(document).ready(async () => {
     $("#learnMore").hide();
     $("#aboutPage").hide();
     $("#escrowPage").hide();
-  
-    // this allows the website to use the metamask account
-    const accounts = await ethereum.enable();
-  
-    web3 = new Web3(ethereum);
-  
-    toWei = (amount) => web3.utils.toWei(String(amount));
-    fromWei = (amount) => Number(web3.utils.fromWei(amount)).toFixed(4);
-  
-    ethereum.on("accountsChanged", (_accounts) => {
-      console.log("Account Changed!", accounts[0]);
-      user = web3.utils.toChecksumAddress(_accounts[0]);
-    });
-  
-    // User will be the first item in the accounts array
-    user = web3.utils.toChecksumAddress(accounts[0]);
-  });
+});  
 
-// Navigation
 $(".marketLink").on("click", function () {
     $("#upload-container").hide();
     $("#deed-container").hide();
